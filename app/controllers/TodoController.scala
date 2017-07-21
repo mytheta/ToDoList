748d522b93ec53a1b920c3cedd99f7c4e186860b
yutsukimiyashita <j148015n@st.u-gakugei.ac.jp>
2017-07-21 23:55:53 +0900

    リスト画面の作成
    これから，DBの設定を行う．
    まだ，Todo1,Todo2しか表示できない．

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -7,11 +7,18 @@ import play.api.data._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 import views._
+import services._
 
 class TodoController @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def helloworld() = Action { implicit request =>
     Ok("Hello World")
+  }
+  def list() = Action { implicit request =>
+    val items: Seq[Todo] = Seq(Todo("Todo1"),Todo("Todo2"))
+    // val message: String = "ここにリストを表示"
+    Ok(html.list(items))
+
   }
 
 }
